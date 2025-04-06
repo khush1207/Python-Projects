cba98a93d7bb3836db04abfb8ae1c6e24c1619de
khush1207 <khushbootalaviya1206@gmail.com>
2025-04-06 13:49:32 +0530

    Add files via upload

---
+++ Web Scraping/main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(url=URL)
+content = response.text
+
+soup = BeautifulSoup(content, "html.parser")
+
+movie_text = [text.getText() for text in soup.find_all(name="h3", class_="title")]
+movie_text.reverse()
+print(movie_text)
+
+#Create a blanck movies.txt file to store the extracted data
+with open("movies.txt", mode="w", encoding="UTF-8") as file:
+    for movie in movie_text:
+        file.write(f"{movie}\n")
